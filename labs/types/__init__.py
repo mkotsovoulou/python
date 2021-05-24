fa6258ba28d65b500ede94451223649759eb543a
Mairako <mkotsovoulou@acg.edu>
2021-05-24 20:19:06 +0300

    Update __init__.py

--- labs/types/__init__.py
+++ labs/types/__init__.py
@@ -8,4 +8,4 @@ def exists(): # the name of the check
 @check50.check(exists) # only run this check if the exists check has passed
 def prints2():
     """Does it print "2" when you input 1 for x and 1 for y ? """
-    check50.run("python3 types.py").stdin(1).stdin(1).stdout("2.0\n").exit(0)
+    check50.run("python3 types.py").stdin("1").stdin("1").stdout("2.0\n").exit(0)
