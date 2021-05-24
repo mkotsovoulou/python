b2260723c862f5b29a37d285246b3719463d2752
Mairako <mkotsovoulou@acg.edu>
2021-05-24 20:15:12 +0300

    Update __init__.py

--- labs/types/__init__.py
+++ labs/types/__init__.py
@@ -2,10 +2,10 @@ import check50 # import the check50 module
 
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
-    """description""" # this is what you will see when running check50
+    """Is your file saved as types.py""" # this is what you will see when running check50
     check50.exists("types.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def prints2():
-    """prints "2?" """
-    check50.run("python3 types.py").stdout("2.0\n", regex=True).exit(0)
+    """Does it print "2" when you input 1 for x and 1 for y ? """
+    check50.run("python3 types.py").stdin(1).stdin(1).stdout("2.0\n").exit(0)
