c295c24f32c418bfc0dc5a90e01fa0c5ecb0bba0
Mairako <mkotsovoulou@acg.edu>
2021-12-12 18:22:03 +0200

    Update __init__.py

--- labs/conditions/__init__.py
+++ labs/conditions/__init__.py
@@ -3,22 +3,22 @@ import check50
 @check50.check()
 def exists():
     """mario.py exists."""
-    check50.exists("mario.py")
+    check50.exists("conditions.py")
 
 @check50.check(exists)
 def correctInput():
     """Returns correct number of hastags """
-    check50.run("python3 mario.py").stdin("1").stdout("#").exit()
-    check50.run("python3 mario.py").stdin("2").stdout("##").exit()
+    check50.run("python3 conditions.py").stdin("1").stdout("#").exit()
+    check50.run("python3 conditions.py").stdin("2").stdout("##").exit()
     
     
 @check50.check(exists)   
 def wrongInput():
-    """Returns wrong grade for invalid grades"""
+    """Returns wrong input """
     from re import match
     
     expected = "[Ww]rong [Nn]umber\n"
-    actual = check50.run("python3 mario.py").stdin("5").stdout()
+    actual = check50.run("python3 conditions.py").stdin("5").stdout()
     if not match(expected, actual):
         help = None
         if match(expected[:-1], actual):
